436b25614ceadf85e0c6d673c0c23ec1e556656c
bublak <pavel.filipcik@gmail.com>
2017-03-20 21:51:42 +0100

    comment edit

--- NamespacePathCreator.py
+++ NamespacePathCreator.py
@@ -14,7 +14,7 @@ class NamespacePathCreator:
             line = line[0:len(line)-1] # get rid off ';' at the end of line
 
         if line.startswith('use '):
-            line = line[4:len(line)] # get rid off 'use '
+            line = line[4:len(line)] # get rid off 'use ' at the beginning of line
 
         line = line.replace('\\', '/') #change path separators
         line = './' + line + '.php'
